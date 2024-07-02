ec40d2e8c41cfa3b7a109314b42a17c0a3bd7d11
Kunal Singh <158199747+RomanKunal@users.noreply.github.com>
2024-07-02 10:40:43 +0530

    committed

---
+++ Backend/Ecommerce/User.model.js
@@ -0,0 +1,25 @@
+const { mongo, Mongoose } = require("mongoose")
+
+import mongoose from 'mongoose'
+
+const UserSchema=new mongoose.Schema({
+  username:{
+    type:String,
+    required:true,
+    lowercase:true,
+    unique:true
+  },
+  email:{
+    type:String,
+    required:true,
+    lowercase:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:[true,"try again"]
+  }
+
+},{timestamps:true})
+
+export const User=mongoose.model('User',UserSchema)
