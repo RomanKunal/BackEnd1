324af9294908643278028080e22662b71fd3b727
RomanKunal <0221csds107@niet.co.in>
2024-07-04 11:37:35 +0530

    committed

---
+++ Backend/Youtube/src/models/User.model.js
@@ -0,0 +1,92 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+//User Schema
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avtar:{
+        type:String, //cloudinarry url aayega 
+        required:true
+    },
+    coverImage:{
+        type:String
+
+    },
+    watchHistory:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Video'
+    }],
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+
+    },
+    refreshToken:{
+        type:String
+    } 
+},{timestamps:true})
+
+UserSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next()
+    }
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+
+//Password check after encryption
+
+UserSchema.methods.isPasswordcorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+UserSchema.methods.generateAccessToken=function(){
+    jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+
+}
+UserSchema.methods.generateRefreshToken=function(){
+    jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY   
+    }
+    )
+}
+
+export const User=mongoose.model('User',UserSchema)
---
+++ Backend/Youtube/src/utils/Cloudinary.js
@@ -0,0 +1,40 @@
+import {v2 as cloudinary} from 'cloudinary'
+import dotenv from 'dotenv'
+import fs from 'fs'
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET  // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary=async(localfilepath)=>{
+    try {
+        if(!localfilepath) return null
+        const response=await cloudinary.uploader.upload(localfilepath,{
+            resource_type:'auto',
+        })
+        console.log(
+            `File uploaded successfully. File public ID is ${response.public_id}`
+        );
+        return response
+    } catch (error) {
+        fs.unlinkSync(localfilepath)// remove the file from the server 
+        return null
+    }
+}
+
+const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+
+export {uploadOnCloudinary}
