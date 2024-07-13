28982970616a439586c39855095f2ba13df4c2d2
RomanKunal <0221csds107@niet.co.in>
2024-07-13 22:46:19 +0530

    chore: Add watch history functionality

--- Backend/Youtube/src/controllers/User.controller.js
+++ Backend/Youtube/src/controllers/User.controller.js
@@ -356,6 +356,54 @@ const getuserchannelprofile=asyncHandler(async(req,res)=>{
 })
 
 
+// getting watch history
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[{
+                                $project:{
+                                    username:1,
+                                    avatar:1,
+                                    fullname:1
+                                }
+                            }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            
+            }
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully"))
+})
+
+
 export {
     registerUser,
     loginUser,
@@ -365,6 +413,8 @@ export {
     getcurrentuser,
     updateavatar,
     updatecoverimage,
-    getuserchannelprofile
+    getuserchannelprofile,
+    updateAccountDetails,
+    getWatchHistory
 
 };
--- Backend/Youtube/src/routes/user.routes.js
+++ Backend/Youtube/src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/User.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, updatepassword, getcurrentuser,updateavatar,
+updatecoverimage,getuserchannelprofile,updateAccountDetails,getWatchHistory } from "../controllers/User.controller.js";
+
 import { upload } from "../middlewares/multer.midlleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const routes = Router();
@@ -15,5 +17,13 @@ routes.route("/login").post(loginUser);
 // secured routes
 routes.route("/logout").post( verifyJWT,logoutUser)
 routes.route("/refresh-token").post(refreshAccessToken)
+routes.route("/change-password").post(verifyJWT,updatepassword)
+routes.route("current-user").get(verifyJWT,getcurrentuser)
+routes.route("/update-avatar").patch(verifyJWT,upload.single('avatar'),updateavatar)
+routes.route("/update-cover-image").patch(verifyJWT,upload.single('coverImage'),updatecoverimage)
+routes.route("/channel-profile").get(getuserchannelprofile)
+routes.route("/update-account-details").patch(verifyJWT,updateAccountDetails)
+routes.route("/watch-history").get(verifyJWT,getWatchHistory)
+
 
 export default routes;
